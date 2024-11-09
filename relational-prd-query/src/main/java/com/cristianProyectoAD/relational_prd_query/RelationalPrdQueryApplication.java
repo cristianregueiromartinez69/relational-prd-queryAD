677ed0f7608946cea4ae751a9e0ab7d55d20d9e3
cristian <cregueiromartinez@danielcastelao.org>
2024-11-09 16:28:21 +0100

    initial commit microservicio postgres

---
+++ relational-prd-query/src/main/java/com/cristianProyectoAD/relational_prd_query/RelationalPrdQueryApplication.java
@@ -0,0 +1,13 @@
+package com.cristianProyectoAD.relational_prd_query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationalPrdQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationalPrdQueryApplication.class, args);
+	}
+
+}
---
+++ relational-prd-query/src/test/java/com/cristianProyectoAD/relational_prd_query/RelationalPrdQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristianProyectoAD.relational_prd_query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationalPrdQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
