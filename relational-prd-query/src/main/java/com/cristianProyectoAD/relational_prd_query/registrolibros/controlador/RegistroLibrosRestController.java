124b7fcbf25d180f31d6e268f1c1898b43dffd41
cristian <cregueiromartinez@danielcastelao.org>
2024-11-10 16:42:18 +0100

    documentacion restController guardado libros en base de postgres

--- relational-prd-query/src/main/java/com/cristianProyectoAD/relational_prd_query/registrolibros/controlador/RegistroLibrosRestController.java
+++ relational-prd-query/src/main/java/com/cristianProyectoAD/relational_prd_query/registrolibros/controlador/RegistroLibrosRestController.java
@@ -5,16 +5,31 @@ import com.cristianProyectoAD.relational_prd_query.registrolibros.servicio.Libro
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * RestController del guardado de libros en postgres
+ * @author cristian
+ * @version 1.0
+ */
 @RestController
 @RequestMapping("/relational-prd-query/libros")
 public class RegistroLibrosRestController {
 
+    //variable del servicio de registro de libros
     private final LibroService libroService;
 
+    /**
+     * Constructor de la clase
+     * @param libroService el servicio de registro de libros
+     */
     public RegistroLibrosRestController(LibroService libroService) {
         this.libroService = libroService;
     }
 
+    /**
+     * Metodo para guardar los libros en la base de datos
+     * @param registro los libros que se registran en la base
+     * @return un mensaje indicando si se realizó el registro
+     */
     @PostMapping("/guardar")
     public ResponseEntity<String> saveBooksPostgres(@RequestBody LibrosRegistroDTO registro) {
         libroService.saveBook(registro);
