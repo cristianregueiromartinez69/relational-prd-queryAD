2dc2ac7455c9dd6554ad8569e1449a0647c2149e
cristian <cregueiromartinez@danielcastelao.org>
2024-11-10 14:33:19 +0100

    creacion de rest controller de registro de libros en postgres

---
+++ relational-prd-query/src/main/java/com/cristianProyectoAD/relational_prd_query/registrolibros/controlador/RegistroLibrosRestController.java
@@ -0,0 +1,23 @@
+package com.cristianProyectoAD.relational_prd_query.registrolibros.controlador;
+
+import com.cristianProyectoAD.relational_prd_query.registrolibros.dto.LibrosRegistroDTO;
+import com.cristianProyectoAD.relational_prd_query.registrolibros.servicio.LibroService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/relational-prd-query/libros")
+public class RegistroLibrosRestController {
+
+    private final LibroService libroService;
+
+    public RegistroLibrosRestController(LibroService libroService) {
+        this.libroService = libroService;
+    }
+
+    @PostMapping("/registrar")
+    public ResponseEntity<String> saveBooksPostgres(@RequestBody LibrosRegistroDTO registro) {
+        libroService.saveBook(registro);
+        return ResponseEntity.ok("Registro de libro en postgresSQL");
+    }
+}
