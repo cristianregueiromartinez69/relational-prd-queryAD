b7f9b49e23ae5783b884a11d8b45e6c6e90709d3
cristian <cregueiromartinez@danielcastelao.org>
2024-11-12 19:44:40 +0100

    cambiando restController de consulta de libros por isbn

--- relational-prd-query/src/main/java/com/cristianProyectoAD/relational_prd_query/consultas/isbn/controlador/LibroConsultaIsbnRestController.java
+++ relational-prd-query/src/main/java/com/cristianProyectoAD/relational_prd_query/consultas/isbn/controlador/LibroConsultaIsbnRestController.java
@@ -2,11 +2,14 @@ package com.cristianProyectoAD.relational_prd_query.consultas.isbn.controlador;
 
 import com.cristianProyectoAD.relational_prd_query.consultas.isbn.servicio.LibroIsbnService;
 import com.cristianProyectoAD.relational_prd_query.registrolibros.modelo.Libros;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/relational-prd-query/libros/consultas")
@@ -20,10 +23,11 @@ public class LibroConsultaIsbnRestController {
 
     @GetMapping("isbn{isbn}")
     public ResponseEntity<Libros> getLibroByIsbn(@PathVariable String isbn) {
-        Libros libro = libroIsbnService.getLibroByIsbn(isbn);
-        if(libro == null) {
-            return ResponseEntity.notFound().build();
+        try {
+            Libros libro = libroIsbnService.getLibroByIsbn(isbn);
+            return ResponseEntity.ok(libro);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
-        return ResponseEntity.ok(libro);
     }
 }
