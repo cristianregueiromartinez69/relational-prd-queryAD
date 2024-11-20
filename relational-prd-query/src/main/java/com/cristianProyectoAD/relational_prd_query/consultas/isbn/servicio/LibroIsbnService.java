e169a9e80db3c8d67e43eddba040f1c5f60a4daf
cristian <cregueiromartinez@danielcastelao.org>
2024-11-20 19:38:10 +0100

    consultas de autor hechas

--- relational-prd-query/src/main/java/com/cristianProyectoAD/relational_prd_query/consultas/isbn/servicio/LibroIsbnService.java
+++ relational-prd-query/src/main/java/com/cristianProyectoAD/relational_prd_query/consultas/isbn/servicio/LibroIsbnService.java
@@ -1,19 +1,35 @@
 package com.cristianProyectoAD.relational_prd_query.consultas.isbn.servicio;
 
-import com.cristianProyectoAD.relational_prd_query.registrolibros.dto.LibrosRegistroDTO;
 import com.cristianProyectoAD.relational_prd_query.registrolibros.modelo.Libros;
-import com.cristianProyectoAD.relational_prd_query.registrolibros.repositorio.LibrosRepositorio;
+import com.cristianProyectoAD.relational_prd_query.repositorio.LibrosRepositorio;
 import org.springframework.stereotype.Service;
 
+/**
+ * Servicio encargado de gestionar las consultas de libros por ISBN.
+ * Interactúa con el repositorio para obtener los datos de los libros en la base de datos.
+ * @author cristian
+ * @version 1.0
+ */
 @Service
 public class LibroIsbnService {
 
     private final LibrosRepositorio librosRepositorio;
 
+    /**
+     * Constructor que inyecta el repositorio de libros.
+     *
+     * @param librosRepositorio El repositorio que gestiona la interacción con la base de datos.
+     */
     public LibroIsbnService(LibrosRepositorio librosRepositorio) {
         this.librosRepositorio = librosRepositorio;
     }
 
+    /**
+     * Obtiene un libro a partir de su ISBN.
+     *
+     * @param isbn El ISBN del libro a consultar.
+     * @return El libro encontrado con el ISBN especificado.
+     */
     public Libros getLibroByIsbn(String isbn) {
         return librosRepositorio.findByIsbn(isbn);
     }
