6973ec6448d457ad234a1619d98a3f85599bfcba
Cristian <cregueiromartinez@danielcastelao.org>
2024-12-02 09:03:44 +0100

    haciendo documentacion

--- relational-prd-query/src/main/java/com/cristianProyectoAD/relational_prd_query/consultas/fecharegistro/Prueba.java
+++
@@ -1,4 +0,0 @@
-package com.cristianProyectoAD.relational_prd_query.consultas.fecharegistro;
-
-public class Prueba {
-}
--- relational-prd-query/src/main/java/com/cristianProyectoAD/relational_prd_query/consultas/isbn/Prueba.java
+++
@@ -1,4 +0,0 @@
-package com.cristianProyectoAD.relational_prd_query.consultas.isbn;
-
-public class Prueba {
-}
