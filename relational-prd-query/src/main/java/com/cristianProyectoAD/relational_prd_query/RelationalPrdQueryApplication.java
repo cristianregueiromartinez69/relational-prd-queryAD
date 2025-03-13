b11d9d545c5ff3049db829b27f49f260afb3e9a5
cristian <cregueiromartinez@danielcastelao.org>
2025-03-13 17:59:28 +0100

    todo funcionando, siii

--- relational-prd-query/src/main/java/com/cristianProyectoAD/relational_prd_query/RelationalPrdQueryApplication.java
+++ relational-prd-query/src/main/java/com/cristianProyectoAD/relational_prd_query/RelationalPrdQueryApplication.java
@@ -10,6 +10,5 @@ public class RelationalPrdQueryApplication {
 		SpringApplication.run(RelationalPrdQueryApplication.class, args);
 	}
 
-	//TODO: preparar consultas para base de datos potgres por isbn
 
 }
